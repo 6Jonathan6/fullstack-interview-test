aae94fac93f376ead7cb3756e8a0a05ae9a47066
6Jonathan6 <jpr1993mm@hotmail.com>
2022-01-13 12:36:12 -0600

    Feature: Fullscreen initial layou

--- flat-frontend/src/components/HelloWorld.vue
+++ flat-frontend/src/components/HelloWorld.vue
@@ -4,15 +4,12 @@
   defineProps<{ msg: string }>();
 
   const count = ref(0);
-  function f() {
-    const a = 10;
-  }
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
+  <p class="flex flex-grow flex-col">
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
@@ -35,21 +32,3 @@
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
-
-<style scoped>
-  a {
-    color: #42b983;
-  }
-
-  label {
-    margin: 0 0.5em;
-    font-weight: bold;
-  }
-
-  code {
-    background-color: #eee;
-    padding: 2px 4px;
-    border-radius: 4px;
-    color: #304455;
-  }
-</style>
